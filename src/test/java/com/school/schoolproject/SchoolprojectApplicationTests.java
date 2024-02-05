3fcabcfbf1027aa2562af3f6423632cd65143194
Onur Gonullu <21060336@stu.omu.edu.tr>
2024-02-06 01:18:21 +0300

    init

---
+++ src/test/java/com/school/schoolproject/SchoolprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.school.schoolproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SchoolprojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
